5a6239931ea02780915998af0772cdc6da375ee4
pandorina1013 <pandorina463@gmail.com>
2017-12-21 16:32:33 +0900

    first commit

---
+++ w2v_urbangarde.R
@@ -0,0 +1,24 @@
+library(RMeCab)
+library(tidyverse)
+library(wordVectors)
+dic_path <- "/Users/svpcadmin/mecab-ipadic-neologd/build/mecab-ipadic-2.7.0-20070801-neologd-20170907/mecab-user-dict-seed.20170907.csv.dic"
+
+w2v_urbangarde <- function(w2v_txt){
+  tf <- tempfile()
+  w2v_txt %>% RMeCabText(dic = dic_path) %>% map(function(x) 
+    ifelse((x[[2]] %in% c("名詞", "形容詞", "動詞")) && 
+             (!x[[3]] %in% c("数", "接尾")) && 
+             (x[[8]] != "*"), 
+           x[[8]], "")
+    ) %>% paste(" ", collapse = "") %>%
+  write(file = tf, append = TRUE)
+  model <- tf %>% train_word2vec("tf.bin", min_count = 2)
+  unlink("tf.bin")
+  unlink(tf)
+  return(model)
+}
+
+w2v_txt <- "urbangarde.txt"
+model <- w2v_urbangarde(w2v_txt)
+
+#model %>% nearest_to(vector = model[["前衛"]], n = 20) %>% round(2) %>% print()
