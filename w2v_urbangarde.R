4ffa0b1134a40fe07779dfac7464ce0cefbc6d3e
Kenta Yamada <pandorina463@gmail.com>
2017-12-24 01:07:52 +0900

    urban lyrics

--- w2v_urbangarde.R
+++ w2v_urbangarde.R
@@ -1,8 +1,11 @@
 library(RMeCab)
 library(tidyverse)
-library(wordVectors)
+#library(wordVectors)
 library(tsne)
+library(rvest)
+
 dic_path <- "/Users/svpcadmin/mecab-ipadic-neologd/build/mecab-ipadic-2.7.0-20070801-neologd-20170907/mecab-user-dict-seed.20170907.csv.dic"
+urban_urls_path = "http://j-lyric.net/artist/a055612/"
 
 w2v_urbangarde <- function(w2v_txt){
   tf <- tempfile()
@@ -19,14 +22,28 @@ w2v_urbangarde <- function(w2v_txt){
   return(model)
 }
 
-#get_lyrics(){
+get_lyrics <- function(urban_urls_path){
   # なんらかの方法で歌詞を取ってきて一つのテキストファイルに追加していく。
   # 敬意を込めてtxtファイルに手打ち...しようと思いましたが、さすがに断念しました。
-#}
+  urban_lyrics <- c()
+  urban_lyric_urls <- urban_urls_path %>% 
+    read_html() %>% 
+    html_nodes("a") %>% 
+    html_attr("href") %>% 
+    paste0("http://j-lyric.net", .) %>% 
+    .[grep("artist",.)] %>% 
+    .[2:length(.)]
+  
+  for(i in 1:length(urban_lyric_urls)){
+    lyrics <- read_html(urban_lyric_urls[i]) %>% html_nodes(xpath = '//*[@id="Lyric"]') %>% html_text()
+    urban_lyrics <- paste0(urban_lyrics, "" ,lyrics)
+    print(lyrics)
+    Sys.sleep(1)
+  }
+  return(urban_lyrics)
+}
 
-w2v_txt <- "urbangarde.txt"
+w2v_txt <- get_lyrics(urban_urls_path)
 model <- w2v_urbangarde(w2v_txt)
-
-#model %>% nearest_to(vector = .[["前衛"]], n = 20) %>% round(2) %>% print()
-
+model %>% nearest_to(vector = .[["前衛"]], n = 20) %>% round(2) %>% print()
 model %>% plot()
